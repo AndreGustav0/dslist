74bb7281aded62cbbee916a1d4dd8e9a404c4431
André Gustavo <andregustavodeoliveira49@gmail.com>
2024-11-13 08:49:53 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/Entities/GameList.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dslist.Entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name= "tb_gameList")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+
+    public GameList(){}
+
+    public GameList(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return id == gameList.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
